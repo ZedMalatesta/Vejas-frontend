8881c35b26df380a0e548ed4944ee6f41e44fcf7
ZedMalatesta <opatriarkh@gmail.com>
2026-05-18 00:32:52 +0300

    feat(player): add basic player

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Room } from '../screens/room/room';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'room', pathMatch: 'full' },
+  { path: 'room', component: Room },
+];
