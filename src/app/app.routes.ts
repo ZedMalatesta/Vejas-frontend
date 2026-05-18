edd26b0b0d94e02d8f198be77c0dc75f583deef7
ZedMalatesta <opatriarkh@gmail.com>
2026-05-19 02:15:08 +0300

    refactor: brought components to the one structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
-import { Room } from '../screens/room/room';
+import { Room } from './screens/room/room';
 import { Login } from './screens/auth/login/login';
 import { Register } from './screens/auth/register/register';
+import { HomePage } from './screens/home-page/home-page';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./pages/home-page/home-page').then((m) => m.HomePage),
-  },
+  { path: '', component: HomePage },
   { path: 'room', component: Room },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
