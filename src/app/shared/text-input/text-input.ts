8d8ca69f8901412c34d2ddd5e64b3303c44ae18a
ZedMalatesta <opatriarkh@gmail.com>
2026-06-21 13:05:38 +0300

    feat: rewrite player logic into reactive forms

--- src/app/shared/text-input/text-input.ts
+++ src/app/shared/text-input/text-input.ts
@@ -1,34 +1,13 @@
-import { Component, effect, input, output, signal } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
+  imports: [ReactiveFormsModule],
   templateUrl: './text-input.html',
   styleUrl: './text-input.scss',
 })
 export class TextInput {
   readonly placeholder = input('');
-  readonly resetKey = input(0);
-  readonly valueChange = output<string>();
-  readonly submit = output<string>();
-
-  readonly value = signal('');
-
-  constructor() {
-    effect(() => {
-      this.resetKey();
-      this.value.set('');
-    });
-  }
-
-  onInput(event: Event): void {
-    const val = (event.target as HTMLInputElement).value;
-    this.value.set(val);
-    this.valueChange.emit(val);
-  }
-
-  onKeydown(event: KeyboardEvent): void {
-    if (event.key === 'Enter') {
-      this.submit.emit(this.value());
-    }
-  }
+  readonly control = input.required<FormControl<string | null>>();
 }
