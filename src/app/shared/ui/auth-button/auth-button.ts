ab93219cd87f45f986dbd80634e2ca1e2cd830af
ZedMalatesta <opatriarkh@gmail.com>
2026-06-22 21:33:55 +0300

    feat: add on push for presentational purposes

--- src/app/shared/ui/auth-button/auth-button.ts
+++ src/app/shared/ui/auth-button/auth-button.ts
@@ -1,9 +1,10 @@
-import { Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-auth-button',
   templateUrl: './auth-button.html',
   styleUrl: './auth-button.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthButton {
   readonly text = input.required<string>();
