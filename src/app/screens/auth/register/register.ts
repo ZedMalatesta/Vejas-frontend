c2974a32acdd167f683f0278f08baf0cfc0e6700
amelur <ameluckin@gmail.com>
2026-06-22 20:08:49 +0300

    feat: add register form and OnPush strategy

--- src/app/screens/auth/register/register.ts
+++ src/app/screens/auth/register/register.ts
@@ -1,21 +1,28 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-
-import { Router, RouterLink } from '@angular/router';
-
-import { AuthService } from '../../../core/services/auth.service';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router, RouterLink} from '@angular/router';
+import {AuthService} from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
   imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: '../shared/auth.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Register {
   authService = inject(AuthService);
   errorMessage = '';
   private fb = inject(FormBuilder);
   form = this.fb.group({
+    username: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(2),
+      ],
+    ],
+
     email: ['', [Validators.required, Validators.email]],
 
     password: ['', [Validators.required, Validators.minLength(6)]],
@@ -31,7 +38,7 @@ export class Register {
     const email = this.form.value.email!;
     const password = this.form.value.password!;
 
-    const { error } = await this.authService.signUp(email, password);
+    const {error} = await this.authService.signUp(email, password);
 
     if (error) {
       this.errorMessage = error.message;
