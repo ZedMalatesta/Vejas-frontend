a6ad3a7518b21b770c2616ddb72b0c73dd9e5b37
ZedMalatesta <opatriarkh@gmail.com>
2026-07-04 15:41:57 +0300

    feat: add rooms rest endpoints

--- server/src/room/room.module.ts
+++ server/src/room/room.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RoomGateway } from './room.gateway';
 import { RoomStateService } from './room-state.service';
+import { RoomsController } from './rooms.controller';
 
 @Module({
+  controllers: [RoomsController],
   providers: [RoomGateway, RoomStateService],
 })
 export class RoomModule {}
