43c3cfd4b1652e0889541cb4ee711b9244a22fa6
Oleksandr Kuzmin <olucens@Oleksandrs-Air.fritz.box>
2026-07-05 00:26:34 +0200

    feat: switch home page to real rooms from backend

--- src/app/screens/home-page/home-page.ts
+++ src/app/screens/home-page/home-page.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ChannelList } from './components/channel-list/channel-list';
+import { RoomList } from './components/room-list/room-list';
 import { Header } from '../../shared/header/header';
 import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ChannelList, Header, Footer, RouterLink],
+  imports: [RoomList, Header, Footer, RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
