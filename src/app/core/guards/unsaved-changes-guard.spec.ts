0a82317feeab7ef66b73a516a78e316d3c4c40f1
Oleksandr Kuzmin <olucens@Oleksandrs-Air.fritz.box>
2026-07-05 00:27:29 +0200

    fix: correct unsaved-changes guard spec typing that broke test build

--- src/app/core/guards/unsaved-changes-guard.spec.ts
+++ src/app/core/guards/unsaved-changes-guard.spec.ts
@@ -1,10 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { CanDeactivateFn } from '@angular/router';
 
-import { unsavedChangesGuard } from './unsaved-changes-guard';
+import { CanComponentDeactivate, unsavedChangesGuard } from './unsaved-changes-guard';
 
 describe('unsavedChangesGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
+  const executeGuard: CanDeactivateFn<CanComponentDeactivate> = (...guardParameters) =>
     TestBed.runInInjectionContext(() => unsavedChangesGuard(...guardParameters));
 
   beforeEach(() => {
